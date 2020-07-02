554e409eb0c08d3abbc2cc8a1ba01f4b3f5c750c
Ahmed Salhin <ahemd.salhin@sage.com>
2020-07-02 15:17:30 +0100

    Initial Commit

---
+++ hfunctions/__init__.py
@@ -0,0 +1,8 @@
+from ._hfunctions import create_sample_df
+from ._hfunctions import random_actual_prediction
+
+
+__author__ = """Ahmed Salhin"""
+__email__ = 'ahmed@salhin.org'
+__version__ = '0.1.0'
+__all__ = ["create_sample_df", "random_actual_prediction"]
